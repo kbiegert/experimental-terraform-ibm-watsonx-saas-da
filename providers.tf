852bb6aa20a27fc1d996d88f209303ce75d393fb
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-04-15 08:43:09 -0400

    set up for testing in staging

--- providers.tf
+++ providers.tf
@@ -1,25 +1,25 @@
 provider "ibm" {
-  alias            = "deployer"
+  #alias            = "deployer"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.location
   visibility       = var.provider_visibility
 }
 
-provider "ibm" {
-  alias            = "watsonx_admin"
-  ibmcloud_api_key = var.watsonx_admin_api_key == null || var.watsonx_admin_api_key == "" ? var.ibmcloud_api_key : var.watsonx_admin_api_key
-  region           = var.location
-  visibility       = var.provider_visibility
-}
+# provider "ibm" {
+#   alias            = "watsonx_admin"
+#   ibmcloud_api_key = var.watsonx_admin_api_key == null || var.watsonx_admin_api_key == "" ? var.ibmcloud_api_key : var.watsonx_admin_api_key
+#   region           = var.location
+#   visibility       = var.provider_visibility
+# }
 
 
-provider "restapi" {
-  alias                = "restapi_watsonx_admin"
-  uri                  = "https:"
-  write_returns_object = true
-  debug                = true
-  headers = {
-    Authorization = data.ibm_iam_auth_token.restapi.iam_access_token
-    Content-Type  = "application/json"
-  }
-}
+# provider "restapi" {
+#   alias                = "restapi_watsonx_admin"
+#   uri                  = "https:"
+#   write_returns_object = true
+#   debug                = true
+#   headers = {
+#     Authorization = data.ibm_iam_auth_token.restapi.iam_access_token
+#     Content-Type  = "application/json"
+#   }
+# }
